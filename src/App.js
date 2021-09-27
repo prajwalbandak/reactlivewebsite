88917137fe5b117860ff33ae63c0ab5cf883d071
abc <abc@example.com>
2021-09-27 22:29:59 +0530

    deployed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Resturant from './components/Basic/Resturant';
+
+
+const App  = () =>{
+  return(
+  <>
+    
+      <Resturant/>
+
+   </>
+  );
+};  
+
+export default App;
